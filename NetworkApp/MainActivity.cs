7bec024350c68bccbd28798c22f517a50e9e4002
Simon <zimme95@gmail.com>
2017-11-03 11:26:09 +0100

    Add project files.

---
+++ NetworkApp/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Core;
+
+namespace NetworkApp
+{
+    [Activity(Label = "NetworkApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            EditText text = FindViewById<EditText>(Resource.Id.addressField);
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+            TextView txtView = FindViewById<TextView>(Resource.Id.textView1);
+
+            button.Click += (sender, e) =>
+            {
+                string newtext = Core.TextChanger.ChangeText(text.Text);
+                txtView.Text = newtext;
+            };
+        }
+    }
+}
+
---
+++ NetworkApp/TextChanger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Core
+{
+    public class TextChanger
+    {
+        public static string ChangeText(string input)
+        {
+            if(string.IsNullOrWhiteSpace(input))
+            {
+                return "";
+            }
+
+            return new string(input.Reverse().ToArray());
+        }
+    }
+}
